824bc3886fdac8f6a30960d30c9330f9008bf2b8
karanpalsinghshekhawat <karanpal609@gmail.com>
2023-01-06 11:01:27 -0500

    split the codebase to pre processing method

--- model/run_main.py
+++ model/run_main.py
@@ -5,14 +5,25 @@ network model and generate parameters and then
 create output file
 """
 
-import datetime
-
 from model import *
 
-if __name__ == "__main__":
-    date = datetime.datetime.today()
+
+def pre_processing():
+    """
+    This method creates the dataset that we will
+    use for training and testing the NN model
+
+    Returns:
+        pd.DataFrame
+    """
     df = read_input_variables_file()
     ticker = df['ticker'].iloc[0]
     st_current_price = get_current_price(ticker)
     range_of_inputs = create_set_of_input_parameters()
     option_df = create_dataset(df, st_current_price, range_of_inputs)
+
+    return option_df
+
+
+if __name__ == "__main__":
+    dt_set = pre_processing()
