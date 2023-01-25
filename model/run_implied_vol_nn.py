9e93bfab1acbc51881f196bd06475d144b8a0e25
karanpalsinghshekhawat <karanpal609@gmail.com>
2023-01-25 17:16:16 -0500

    minor restructure, model completed

--- model/run_implied_vol_nn.py
+++ model/run_implied_vol_nn.py
@@ -22,12 +22,11 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--num_dt_training', type=int, help='Actual model run dataset size')
     args = parser.parse_args()
     df, st_current_price, range_of_inputs = pre_processing(args.num_dt_training, "BS")
-    big_dataset = create_heston_dataset(df, st_current_price, range_of_inputs)
+    big_dataset = create_implied_vol_dataset(df, st_current_price, range_of_inputs)
     file_name = r"./model/output/" + "best_hyper_parameter.pkl"
     df_hyper = pd.read_pickle(file_name)
-    feature_columns = ['moneyness', 'time_to_maturity', 'risk_free_rate', 'correlation', 'reversion_speed',
-                       'Long_average_variance', 'vol_vol', 'initial_variance']
-    target = 'opt_price_by_strike'
+    feature_columns = ['moneyness', 'time_to_maturity', 'risk_free_rate', 'scaled_time_value']
+    target = 'volatility'
     model = run_nn_model(big_dataset, df_hyper, feature_columns, target)
-    model_save_path = r"./model/output/" + "Heston_NN_model.h5"
+    model_save_path = r"./model/output/" + "implied_vol_NN_model.h5"
     model.save(model_save_path)
