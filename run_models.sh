fa6df8a928fc289dd80dc16f7b7b4c747d3e7592
karanpalsinghshekhawat <karanpal609@gmail.com>
2023-01-29 14:29:38 -0500

    implied volatility model built

--- run_models.sh
+++ run_models.sh
@@ -1,1 +1,3 @@
-python -m model.run_pricing -h 50000 -t 1000000
+python -m model.run_bs_nn -h 50000 -t 100000
+python -m model.run_heston_nn -t 1000000
+python -m model.run_implied_vol_nn -t 1000000
